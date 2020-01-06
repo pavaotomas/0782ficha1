dcc0c4d5ca61a1f68c76814237f835acecb23e7f
pavaotomas <tomasamaralpavao@enta.pt>
2020-01-06 11:23:18 +0000

    Para o X-Jorgim

--- prog3.c
+++ prog3.c
@@ -7,13 +7,15 @@ void main(){
     float iva=0.18;
     float fim;
 
+//output e input do consumo de eletricidade
     printf("Insere o consumo de eletricidade:\n");
 
     scanf("%f", &consumo);
-
+//processo de calculo do valor de pagamento
     fim = potencia + consumo * kwh;
     fim = fim * iva + fim;
-
+    
+//output do valor depagamento
     printf("O valor a pagar é: %.2f\n", fim);
 
 }
--- prog4.c
+++ prog4.c
@@ -7,20 +7,25 @@ void main(){
     float teste3;
     float media;
 
+//output e input do nome
 printf("Insere o teu primeiro nome:\n");
 
 scanf("%s", nome);
 
+//output e input do teste1
 printf("Insere a nota do teste 1:");
 scanf("%f", &teste1);
 
+//output e input do teste2
 printf("Insere a nota do teste 2:");
 scanf("%f", &teste2);
-
+// output e input do teste3
 printf("Insere a nota do teste 3:");
 scanf("%f", &teste3);
 
+//processo de calculo da media
 media = teste1*0.25 + teste2*0.35 + teste3*0.40;
 
+//output do nome e media final
 printf("Nome: %s \n Média: %.2f \n", nome, media);
 }
